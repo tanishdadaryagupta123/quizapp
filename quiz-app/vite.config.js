931cf52fc86bd7807156ccdac401a0526c598c2e
Tanish Dadarya Gupta <114875082+tanishdadaryagupta123@users.noreply.github.com>
2025-01-20 22:19:56 +0530

    quizapp

--- quiz-app/vite.config.js
+++ quiz-app/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Export Vite configuration
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -8,8 +9,13 @@ export default defineConfig({
       '/api': {
         target: 'https://opentdb.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: 'jsx', // Ensures JSX files are processed correctly by Vite’s bundler (esbuild)
+    },
+  },
 })
